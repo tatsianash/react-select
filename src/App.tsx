5cd288f8b7a54114098b1d02f9c8a00e08d67ff2
Tatsiana Shapavalava <tatsiana_shapavalava1@epam.com>
2023-01-27 09:09:31 -0700

    Add layout styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Select from './components/Select';
 
 function App() {
@@ -6,10 +7,31 @@ function App() {
     { label: 'Second', value: 'Second' },
     { label: 'Third', value: 3 },
     { label: 'Forth', value: 4 },
+    { label: '5th', value: 5 },
+    { label: '6th', value: 6 },
+    { label: '7th', value: 7 },
+    { label: '8th', value: 8 },
+    // { label: 'First', value: 'First' },
+    // { label: 'Second', value: 'Second' },
+    // { label: 'Third', value: 3 },
+    // { label: 'Forth', value: 4 },
+    // { label: '5th', value: 5 },
+    // { label: '6th', value: 6 },
+    // { label: '7th', value: 7 },
+    // { label: '8th', value: 8 },
+    // { label: 'First', value: 'First' },
+    // { label: 'Second', value: 'Second' },
+    // { label: 'Third', value: 3 },
+    // { label: 'Forth', value: 4 },
+    // { label: '5th', value: 5 },
+    // { label: '6th', value: 6 },
+    // { label: '7th', value: 7 },
+    // { label: '8th', value: 8 },
   ];
+  const [value, setValue] = useState<typeof options[0] | undefined>(options[1]);
   return (
     <>
-      <Select options={options} />
+      <Select options={options} value={value} onChange={(o) => setValue(o)} />
     </>
   );
 }
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -14,14 +14,14 @@ type SelectProps = {
 
 export default function Select({ value, onChange, options }: SelectProps) {
   return (
-    <div className={styles.container}>
-      <span className={styles.value}>value</span>
+    <div className={styles.container} tabIndex={0}>
+      <span className={styles.value}>{value?.label}</span>
       <button className={styles['clear-btn']}>&times;</button>
       <div className={styles.divider}></div>
       <div className={styles.caret}></div>
-      <ul className={styles.options}>
+      <ul className={`${styles.options} ${styles.show}`}>
         {options.map((o) => (
-          <li key={o.value} className={styles.option}>
+          <li key={o.value} className={`${styles.option}`}>
             {o.label}
           </li>
         ))}
